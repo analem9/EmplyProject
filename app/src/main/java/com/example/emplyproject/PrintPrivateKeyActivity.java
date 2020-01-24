28ee2347d5fa9478c5f25de31d5182682c682520
analem9 <anais.nalem@laposte.net>
2020-01-24 15:55:23 +0100

    first commit

---
+++ app/src/main/java/com/example/emplyproject/PrintPrivateKeyActivity.java
@@ -0,0 +1,15 @@
+package com.example.emplyproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrintPrivateKeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //String privatekey = "test";
+        //Log.d("###", privatekey);
+    }
+}
