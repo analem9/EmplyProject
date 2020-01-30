282ddba6072f757691782e74a4da84027362f8e3
analem9 <anais.nalem@laposte.net>
2020-01-30 12:02:12 +0100

    add print hashmap key, value

--- app/src/main/java/com/example/emplyproject/PrintPrivateKeyActivity.java
+++ app/src/main/java/com/example/emplyproject/PrintPrivateKeyActivity.java
@@ -4,20 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class PrintPrivateKeyActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  //  @Override
+   // protected void onCreate(Bundle savedInstanceState) {
+  //      super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //String privatekey = "test";
         //Log.d("###", privatekey);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // Intent intent = getIntent();
+        // String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         //TextView textView = findViewById(R.id.textView);
         //textView.setText(message);
+
+    public void Printkey() {
+
+        HashMap<Integer, Integer>  privatekey = new HashMap<Integer, Integer>();    // test example of hashmap formatted data
+        privatekey.put(1, 21);
+        privatekey.put(2, 45);
+
+        Iterator<Integer> keySetIterator = privatekey.keySet().iterator();
+
+        while(keySetIterator.hasNext()) {
+            Integer key = keySetIterator.next();
+            System.out.println(key.toString() + " : " + privatekey.get(key).toString());    // display key and value, string formatted
+        }
+
 
     }
 
