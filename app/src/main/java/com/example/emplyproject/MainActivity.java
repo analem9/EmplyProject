5eba559d23efb3830a2f84d3b9572364f4f71be1
analem9 <anais.nalem@laposte.net>
2020-01-31 10:51:02 +0100

    call printkey() from MainActivity

--- app/src/main/java/com/example/emplyproject/MainActivity.java
+++ app/src/main/java/com/example/emplyproject/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.content.Context;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -19,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, PrintPrivateKeyActivity.class);
-        String message = "private key";
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+        //String message = "private key";
+        //intent.putExtra(EXTRA_MESSAGE,message);
+        //startActivity(intent);
+
+        PrintPrivateKeyActivity PK = new PrintPrivateKeyActivity(PrintPrivateKeyActivity.context);
+
+        String key = PK.Printkey();
+
+        Toast.makeText(MainActivity.this, key, Toast.LENGTH_LONG).show();
+
     }
 
 }
--- app/src/main/java/com/example/emplyproject/PrintPrivateKeyActivity.java
+++ app/src/main/java/com/example/emplyproject/PrintPrivateKeyActivity.java
@@ -9,33 +9,35 @@ import java.util.Iterator;
 
 public class PrintPrivateKeyActivity extends AppCompatActivity {
 
-  //  @Override
-   // protected void onCreate(Bundle savedInstanceState) {
-  //      super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //String privatekey = "test";
-        //Log.d("###", privatekey);
+    //  @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //      super.onCreate(savedInstanceState);
+    //setContentView(R.layout.activity_main);
+    //String privatekey = "test";
+    //Log.d("###", privatekey);
 
-        // Intent intent = getIntent();
-        // String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    // Intent intent = getIntent();
+    // String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        //TextView textView = findViewById(R.id.textView);
-        //textView.setText(message);
+    //TextView textView = findViewById(R.id.textView);
+    //textView.setText(message);
 
-    public void Printkey() {
+    public String Printkey() {
 
-        HashMap<Integer, Integer>  privatekey = new HashMap<Integer, Integer>();    // test example of hashmap formatted data
+        HashMap<Integer, Integer> privatekey = new HashMap<Integer, Integer>();    // test example of hashmap formatted data
         privatekey.put(1, 21);
         privatekey.put(2, 45);
 
         Iterator<Integer> keySetIterator = privatekey.keySet().iterator();
+        String igotthekey;
 
-        while(keySetIterator.hasNext()) {
+        while (keySetIterator.hasNext()) {
             Integer key = keySetIterator.next();
             System.out.println(key.toString() + " : " + privatekey.get(key).toString());    // display key and value, string formatted
+            //String igotthekey =  igoythekey.concat(key.toString() + " : " + privatekey.get(key).toString() +'\n');
         }
-
-
+    return "end";
     }
-
 }
+
+
